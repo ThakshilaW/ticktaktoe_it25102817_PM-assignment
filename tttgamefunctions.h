c3768a52d18f97c5688e24b909d85b781fdacfc6
ThakshilaW <it25102817@my.sliit.lk>
2025-10-05 17:32:37 +0530

    corrected prev header+ new header

--- tttgamefunctions.h
+++ tttgamefunctions.h
@@ -1,5 +1,10 @@
-#ifndef TICTACTOE_H
-#define TICTACTOE_H
+#ifndef TTTGAMEFUNCTIONS_H
+#define TTTGAMEFUNCTIONS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
 
 //function declarations
 char** Board(int size); //changed function name than earlier commits because why not
